9ccd02e376b90a0aa7aa5724f051f0dde91fff19
Peter <peter.ishado@gmail.com>
2025-11-04 09:48:56 +0100

    chore: cleaned up installer form three

--- apps/saas/src/pages/installer-onboarding-form/step-three.tsx
+++ apps/saas/src/pages/installer-onboarding-form/step-three.tsx
@@ -85,8 +85,8 @@ const InstallerOnboardingFormThree = () => {
   ];
 
   return (
-    <div className="w-full mx-auto flex flex-col md:!items-center md:!justify-center max-w-[375px] md:!max-w-[1440px] h-fit bg-[#F4F4F4]">
-      <div className="flex flex-col w-full gap-6 md:!max-w-[1076px] h-fit p-[40px] bg-[#FFFFFF]">
+    <div className="w-full mx-auto flex flex-col items-center justify-center  h-fit bg-[#F4F4F4]">
+      <div className="flex flex-col w-full gap-6 max-w-[1076px] h-fit  p-[40px] bg-[#FFFFFF]">
         <div className="flex flex-col items-center">
           <div className="w-fit md:!mb-6">
             <Image
@@ -106,181 +106,172 @@ const InstallerOnboardingFormThree = () => {
 
         <FormikProvider value={formik}>
           <Form onSubmit={handleSubmit}>
-            <div className="flex flex-col w-full md:!max-w-[795px] max-w-[345px] h-fit">
-              <div className="flex w-full max-w-[345px] md:!ml-[20%] md:!max-w-[795px] gap-[10px]">
-                <FieldArray name="projects">
-                  {({ push, remove }) => (
-                    <div>
-                      {formik.values.projects.map((project, index) => (
-                        <div className="flex flex-col ml-[13%] md:!flex-row gap-[10px] w-full max-w-[345px] md:!max-w-[700px]">
-                          <div
-                            key={index}
-                            className="flex flex-col md:!flex-row gap-[10px]"
-                          >
-                            <div className="flex flex-col border border-[#C1C6C5]/50 pt-[20px] pr-[10px] pb-[20px] pl-[10px] gap-[30px] md:!gap-[50px] rounded-[10px] w-full h-fit md:!max-w-[688px] mt-4">
-                              {/* Fields */}
-                              <div className="grid grid-cols-1 gap-[30px] md:!grid-cols-2 md:!gap-[81px] w-full md:!max-w-[688px] h-fit">
-                                <div>
-                                  <Typography.body1 weight="semibold">
-                                    Project Location
-                                  </Typography.body1>
-                                  <InputField.primary
-                                    className="w-full rounded-[10px] md:!max-w-[285px]"
-                                    name={`projects[${index}].projectLocation`}
-                                    placeholder="Project Location"
-                                    rounded="full"
-                                    value={project.projectLocation}
-                                    onChange={formik.handleChange}
-                                    validate
-                                  />
-                                </div>
-
-                                <div>
-                                  <Typography.body1 weight="semibold">
-                                    Energy Consumption Preference
-                                  </Typography.body1>
-
-                                  <MultiSelectInput.primary
-                                    rounded="full"
-                                    className="rounded-full"
-                                    placeholder="Fan, Fridge, Oven"
-                                    name={`projects[${index}].frameworks`}
-                                    options={frameworks}
-                                    value={
-                                      formik.values.projects[index].frameworks
-                                    }
-                                    onChange={(selected) =>
-                                      formik.setFieldValue(
-                                        `projects[${index}].frameworks`,
-                                        selected
-                                      )
-                                    }
-                                    validate
-                                  />
-                                </div>
-                              </div>
-
-                              {/* Upload Images */}
-                              <div className="flex flex-col">
-                                <Typography.body1 weight="semibold">
-                                  Upload Photos
-                                </Typography.body1>
-                                <FieldArray name={`projects[${index}].images`}>
-                                  {({
-                                    push: pushImage,
-                                    remove: removeImage,
-                                  }) => (
-                                    <>
-                                      <UploadField
-                                        containerProps={{
-                                          className:
-                                            "flex-1 w-full max-w-[102px] md:!max-w-[232px] md!h-[61px]",
-                                        }}
-                                        showUploadList={false}
-                                        fieldProps={{
-                                          name: `projects[${index}].images`,
-                                          multiple: true,
-                                          accept: "image/*",
-                                          onChange: (e) => {
-                                            Array.from(
-                                              e.target.files || []
-                                            ).forEach((file) => {
-                                              fileToBase64(file).then(
-                                                (base64) => {
-                                                  pushImage({
-                                                    name: file.name,
-                                                    url: base64,
-                                                  });
-                                                }
-                                              );
-                                            });
-                                          },
-                                        }}
-                                        validate
-                                      />
-
-                                      <ComponentVisibility
-                                        visible={project.images?.length > 0}
-                                      >
-                                        <div className="flex gap-5 mt-2">
-                                          {project.images?.map((img, i) => (
-                                            <div key={i} className="relative">
-                                              <Image
-                                                containerClassName="size-[60px]"
-                                                src={img.url}
-                                              />
-                                              <button
-                                                type="button"
-                                                onClick={() => removeImage(i)}
-                                                className="absolute -top-2 -right-2 bg-gray-200 rounded-full p-1"
-                                              >
-                                                <XIcon className="w-4 h-4 text-red-500" />
-                                              </button>
-                                            </div>
-                                          ))}
-                                        </div>
-                                      </ComponentVisibility>
-                                    </>
-                                  )}
-                                </FieldArray>
-                              </div>
-
-                              {/* Caption */}
-                              <div>
-                                <Typography.body1 weight="semibold">
-                                  Caption
-                                </Typography.body1>
-                                <TextAreaField.primary
-                                  className="w-full h-[90px]"
-                                  name={`projects[${index}].caption`}
-                                  value={project.caption}
-                                  onChange={formik.handleChange}
-                                  placeholder="Write a caption"
-                                />
-                              </div>
+            <div className="flex flex-col gap-3  w-full max-w-[300px] md:!max-w-[850px] mx-auto h-fit lg:!py-[20px]">
+              <FieldArray name="projects">
+                {({ push, remove }) => (
+                  <div className="flex flex-col md:!ml-[13%]  gap-[10px] w-full max-w-[345px] md:!max-w-[700px]">
+                    {formik.values.projects.map((project, index) => (
+                      <div
+                        key={index}
+                        className="flex flex-col md:!flex-row gap-[10px]"
+                      >
+                        <div className="flex flex-col border border-[#C1C6C5]/50 pt-[40px] pr-[20px] pb-[40px] pl-[20px] gap-[30px] md:!gap-[50px] rounded-[10px] w-full h-fit md:!max-w-[688px] mt-4">
+                          {/* Fields */}
+                          <div className="grid grid-cols-1 gap-[30px] md:!grid-cols-2 md:!gap-[81px] w-full md:!max-w-[688px] h-fit">
+                            <div>
+                              <Typography.body1 weight="semibold">
+                                Project Location
+                              </Typography.body1>
+                              <InputField.primary
+                                className="w-full rounded-[10px] md:!max-w-[285px]"
+                                name={`projects[${index}].projectLocation`}
+                                placeholder="Project Location"
+                                rounded="full"
+                                value={project.projectLocation}
+                                onChange={formik.handleChange}
+                                validate
+                              />
                             </div>
-                            {/* Add / Remove */}
-                            <div className="flex gap-4  h-[43px] md:!mt-[126px]">
-                              {index === 0 ? (
-                                <button
-                                  type="button"
-                                  onClick={() =>
-                                    push({
-                                      projectLocation: "",
-                                      loads: "",
-                                      caption: "",
-                                      frameworks: [],
-                                      images: [],
-                                    })
-                                  }
-                                  className="border flex items-center gap-2 rounded-[50px] px-3 py-2 border-[#2495D1]"
-                                >
-                                  <CrossIcon />
-                                  <Typography.body1 className="text-[#2495D1]">
-                                    Add
-                                  </Typography.body1>
-                                </button>
-                              ) : (
-                                <button
-                                  type="button"
-                                  onClick={() => remove(index)}
-                                  className="border flex items-center justify-center rounded-[50px] px-3 py-2 border-[#E3140F]"
-                                >
-                                  <DeleteIcon />
-                                </button>
-                              )}
+
+                            <div>
+                              <Typography.body1
+                                weight="semibold"
+                                className="tracking-[1%]"
+                              >
+                                Energy Consumption Preference
+                              </Typography.body1>
+                              <MultiSelectInput.primary
+                                placeholder="Fan, Fridge, Oven"
+                                name={`projects[${index}].frameworks`}
+                                options={frameworks}
+                                value={formik.values.projects[index].frameworks}
+                                onChange={(selected) =>
+                                  formik.setFieldValue(
+                                    `projects[${index}].frameworks`,
+                                    selected
+                                  )
+                                }
+                                validate
+                              />
                             </div>
                           </div>
+
+                          {/* Upload Images */}
+                          <div className="flex flex-col">
+                            <Typography.body1 weight="semibold">
+                              Upload Photos
+                            </Typography.body1>
+                            <FieldArray name={`projects[${index}].images`}>
+                              {({ push: pushImage, remove: removeImage }) => (
+                                <>
+                                  <UploadField
+                                    containerProps={{
+                                      className:
+                                        "flex-1 w-full max-w-[277px] md!h-[61px]",
+                                    }}
+                                    showUploadList={false}
+                                    fieldProps={{
+                                      name: `projects[${index}].images`,
+                                      multiple: true,
+                                      accept: "image/*",
+                                      onChange: (e) => {
+                                        Array.from(
+                                          e.target.files || []
+                                        ).forEach((file) => {
+                                          fileToBase64(file).then((base64) => {
+                                            pushImage({
+                                              name: file.name,
+                                              url: base64,
+                                            });
+                                          });
+                                        });
+                                      },
+                                    }}
+                                    validate
+                                  />
+
+                                  <ComponentVisibility
+                                    visible={project.images?.length > 0}
+                                  >
+                                    <div className="flex gap-5 mt-2">
+                                      {project.images?.map((img, i) => (
+                                        <div key={i} className="relative">
+                                          <Image
+                                            containerClassName="size-[60px]"
+                                            src={img.url}
+                                          />
+                                          <button
+                                            type="button"
+                                            onClick={() => removeImage(i)}
+                                            className="absolute -top-2 -right-2 bg-gray-200 rounded-full p-1"
+                                          >
+                                            <XIcon className="w-4 h-4 text-red-500" />
+                                          </button>
+                                        </div>
+                                      ))}
+                                    </div>
+                                  </ComponentVisibility>
+                                  <Typography.body2 className="tracking-[1%] text-[#5A5F61] mt-5">
+                                    PNG, JPG, Only
+                                  </Typography.body2>
+                                </>
+                              )}
+                            </FieldArray>
+                          </div>
+
+                          {/* Caption */}
+                          <div>
+                            <Typography.body1 weight="semibold">
+                              Caption
+                            </Typography.body1>
+                            <TextAreaField.primary
+                              className="w-full h-[90px]"
+                              name={`projects[${index}].caption`}
+                              value={project.caption}
+                              onChange={formik.handleChange}
+                              placeholder="Write a caption"
+                            />
+                          </div>
                         </div>
-                      ))}
-                    </div>
-                  )}
-                </FieldArray>
-              </div>
+                        {/* Add / Remove */}
+                        <div className="flex gap-4  h-[43px] md:!mt-[126px] w-full max-w-[20px]">
+                          {index === 0 ? (
+                            <button
+                              type="button"
+                              onClick={() =>
+                                push({
+                                  projectLocation: "",
+                                  loads: "",
+                                  caption: "",
+                                  frameworks: [],
+                                  images: [],
+                                })
+                              }
+                              className="border flex items-center gap-2 rounded-[50px] px-3 py-2 border-[#2495D1]"
+                            >
+                              <CrossIcon />
+                              <Typography.body1 className="text-[#2495D1]">
+                                Add
+                              </Typography.body1>
+                            </button>
+                          ) : (
+                            <button
+                              type="button"
+                              onClick={() => remove(index)}
+                              className="border flex items-center justify-center rounded-[50px] px-3 py-2 border-[#E3140F]"
+                            >
+                              <DeleteIcon />
+                            </button>
+                          )}
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </FieldArray>
 
               <Button.PrimarySolid
-                rounded="md"
-                className="w-full max-w-[90px] self-center rounded-[10px] md!rounded-full md:!self-end md:!max-w-[290px] h-12 text-white mt-6"
+                className="w-full max-w-[150px] self-center md:!ml-[50%]  md:!max-w-[290px] h-12 text-white mt-6"
                 type="submit"
               >
                 Continue
