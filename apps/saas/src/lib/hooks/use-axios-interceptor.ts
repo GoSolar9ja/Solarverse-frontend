c93091f1a23c2c5e0874601ef00b32c7ecbb2432
Eugene44-hub <eugeneishado44@gmail.com>
2025-11-09 14:03:38 +0100

    feat(saas): add axios interceptor hook for global error handling
    - Create useAxiosInterceptor hook to handle 401 errors and show toast messages
    - Integrate useAxiosInterceptor in AppRoutes to apply interceptor globally
    - Update axiosInstance to use environment variable for baseURL
    - Remove old response interceptor logic from axiosInstance
    - Add dev:prod script and extend .gitignore for .env files in saas app package.json

---
+++ apps/saas/src/lib/hooks/use-axios-interceptor.ts
@@ -0,0 +1,40 @@
+import { toast } from "sonner";
+import { useAuthContext } from "../providers/context-provider/auth-provider";
+import axiosInstance from "../services/config/axios-instance";
+import { useEffect } from "react";
+import { getFromLocalStorage } from "../utils/local-storage";
+import { STORAGE_KEYS } from "../constants";
+
+const useAxiosInterceptor = () => {
+  const { logout } = useAuthContext() || {};
+
+  useEffect(() => {
+    const interceptor = axiosInstance.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        if (error.response?.status === 401) {
+          const token = getFromLocalStorage(STORAGE_KEYS.GOSOLAR_TOKEN);
+          if (token) {
+            logout?.();
+            console.log("Log User out");
+          }
+        }
+        if (error.response?.status !== 200) {
+          console.log("interceptor", error.response?.data);
+          const errorMessage =
+            error.response?.data.message ||
+            error.response?.data.data?.message ||
+            error.response?.data?.detail;
+          if (errorMessage) {
+            toast.error(errorMessage);
+          }
+        }
+
+        return Promise.reject(error);
+      }
+    );
+    return () => axiosInstance.interceptors.response.eject(interceptor);
+  }, []);
+};
+
+export default useAxiosInterceptor;
--- apps/saas/src/lib/routes/index.tsx
+++ apps/saas/src/lib/routes/index.tsx
@@ -1,8 +1,10 @@
 import { RouteObject, useRoutes } from "react-router-dom";
 import publicRoutes from "./public-routes";
 import protectedRoutes from "./protected-routes";
+import useAxiosInterceptor from "../hooks/use-axios-interceptor";
 
 export default function AppRoutes() {
+  useAxiosInterceptor();
   return useRoutes(publicRoutes.concat(protectedRoutes).concat(notFoundRoute));
 }
 
